8cd4da26a1faed070a8979a23d536ff4f50f805b
Aladesigncs <mrRobot13z@proton.me>
2025-01-27 16:08:32 +0300

    load env

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'GRADUATION')  # Get secret key from environment variables
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL',
-        'mysql+pymysql://ahmed:12345678@localhost/graduation'  
+        'mysql+pymysql://ahmed:12345678@data/graduation'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable SQLAlchemy event system
     ADMIN_REGISTRATION_TOKEN = os.environ.get('ADMIN_REGISTRATION_TOKEN', 'GRADUATION')  # Get admin registration token from environment variables
