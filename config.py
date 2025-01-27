a58ec356b259b0b3ea81aaecebec6874b73a0c1a
Aladesigncs <mrRobot13z@proton.me>
2025-01-28 00:05:08 +0300

    adding font awsome packages

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'GRADUATION')  # Get secret key from environment variables
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL',
-        'mysql+pymysql://ahmed:12345678@data/graduation'
+        'mysql+pymysql://ahmed:12345678@127.0.0.1/graduation'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable SQLAlchemy event system
     ADMIN_REGISTRATION_TOKEN = os.environ.get('ADMIN_REGISTRATION_TOKEN', 'GRADUATION')  # Get admin registration token from environment variables
