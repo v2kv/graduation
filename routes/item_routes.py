8e4834209bbf3999618883511f86bf90296f916f
Aladesigncs <mrRobot13z@proton.me>
2025-04-04 20:38:03 +0300

    item_details.html changes

--- routes/item_routes.py
+++ routes/item_routes.py
@@ -7,10 +7,10 @@ item_bp = Blueprint('item', __name__)
 def item_list():
     items = Item.query.options(joinedload(Item.images)).all()
     categories = Category.query.all()  # Add categories for filtering
-    return render_template('item_list.html', items=items, categories=categories)
+    return render_template('item_list.html', items=items, categories=categories,show_footer=True)
 
 @item_bp.route('/item/<int:item_id>')
 def item_detail(item_id):
     item = Item.query.options(joinedload(Item.images)).get_or_404(item_id)
     print(f"DEBUG: Item details {item.__dict__}")  # Prints all attributes
-    return render_template('item_detail.html', item=item)
+    return render_template('item_detail.html', item=item,show_footer=True)
